67f99e70389d351419067891ee9f92a1aec16ac6
Ahmed Ibrahim <Ahmed.Ahmed.Ibrahim2003@gmail.com>
2023-12-21 02:23:53 +0200

    final commit

--- map/script.js
+++ map/script.js
@@ -1,10 +1,9 @@
 document.querySelectorAll(".allPaths").forEach(e => {
-    e.setAttribute('class', `allPaths ${e.id}`);
     e.addEventListener("mouseover", function () {
         window.onmousemove = function (j) {
             x = j.pageX
             y = j.pageY
-            document.getElementById('name').style.top = y - 60 + 'px'
+            document.getElementById('name').style.top = y - 40 + 'px'
             document.getElementById('name').style.left = x + 10 + 'px'
         }
         document.getElementById("name").style.opacity = 1
@@ -18,10 +17,10 @@ document.querySelectorAll(".allPaths").forEach(e => {
         const url = `/${e.id}/index.html`;
         fetch(url)
             .then(response => {
-                if (!response.ok) {
-                    window.location.assign("/Under-Dev/index.html");
-                } else {
+                if (response.ok) {
                     window.location.assign(url);
+                } else {
+                    window.location.assign("/Under-Dev/index.html");
                 }
             })
     });
@@ -33,11 +32,11 @@ let button2 = document.getElementById("get-started2");
 
 let contact = document.getElementById("contact-btn");
 
-button.addEventListener("click", () => window.scrollTo({ top: 960, behavior: 'smooth' })
+button.addEventListener("click", () => window.scrollTo({ top: 1031, behavior: 'smooth' })
 );
 
-button2.addEventListener("click", () => window.scrollTo({ top: 960, behavior: 'smooth' })
+button2.addEventListener("click", () => window.scrollTo({ top: 1031, behavior: 'smooth' })
 );
 
-contact.addEventListener("click", () => window.scrollTo({ top: 1500, behavior: 'smooth' })
+contact.addEventListener("click", () => window.scrollTo({ top: 2000, behavior: 'smooth' })
 );
